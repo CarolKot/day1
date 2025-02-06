16e6d57a65eb8f16928a6faafa6dcf5897c0464c
CarolKot <kerolkot@yandex.by>
2025-02-06 21:26:28 +0300

    Введите пароль

---
+++ password.py
@@ -0,0 +1,8 @@
+password = int(input("Введите пароли "))
+while password != 1234:
+    print("пфроль не верный")
+    password = int(input("Введите пароли "))
+else:
+    print("пфроль верный")
+       
+

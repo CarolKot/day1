e0bb8b2301c53ce0e43f80bec2af6dd8f7f28c9f
CarolKot <kerolkot@yandex.by>
2025-02-07 17:57:56 +0300

    temperature.py

--- temperature.py
+++ temperature.py
@@ -1,25 +1,18 @@
 temperature = int(input("Какая температу на датчике? "))
 double = 0
 problem = 0 
-reat_chek = False
-while double != temperature:
-    print(f'{temperature}')
-    double = temperature
-    temperature = int(input("Какая температу на датчике? "))
-    if temperature == double:
-        problem +=1
-        print(f'Внимание: дублирующее значение температуры {double} обнаружено!')
-        print(f'Зафиксировано сбоев датчика:{problem}')
-        question = int(input("Хотите продолжить сбор данных? "))
-        if question == 0:
-            print(f'Сбор данных остановлен')
-            break
-            
-        elif question == 1:
-            reat_chek = True
-            temperature = int(input("Какая температу на датчике? "))
-
-
-            
-
-
+while True:
+    if double != temperature:
+        print(f'{temperature}')
+        double = temperature
+        temperature = int(input("Какая температу на датчике? "))
+        if temperature == double:
+            problem +=1
+            print(f'Внимание: дублирующее значение температуры {double} обнаружено!')
+            print(f'Зафиксировано сбоев датчика:{problem}')
+            question = int(input("Хотите продолжить сбор данных? "))
+            if question == 0:
+               print(f'Сбор данных остановлен')
+               break
+            elif question == 1:
+                temperature = int(input("Какая температу на датчике? "))

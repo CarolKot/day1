d404984d148e11623e3d5723862cdd4f437f3b55
CarolKot <kerolkot@yandex.by>
2025-02-07 17:32:46 +0300

    Зупер ФЛАГ-Решено

---
+++ temperature.py
@@ -0,0 +1,25 @@
+temperature = int(input("Какая температу на датчике? "))
+double = 0
+problem = 0 
+reat_chek = False
+while double != temperature:
+    print(f'{temperature}')
+    double = temperature
+    temperature = int(input("Какая температу на датчике? "))
+    if temperature == double:
+        problem +=1
+        print(f'Внимание: дублирующее значение температуры {double} обнаружено!')
+        print(f'Зафиксировано сбоев датчика:{problem}')
+        question = int(input("Хотите продолжить сбор данных? "))
+        if question == 0:
+            print(f'Сбор данных остановлен')
+            break
+            
+        elif question == 1:
+            reat_chek = True
+            temperature = int(input("Какая температу на датчике? "))
+
+
+            
+
+
